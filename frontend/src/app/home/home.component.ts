5243d528f84bb7c0e21749daa80fbf6348666e12
RafaelCMartins <rafaelYMG0@gmail.com>
2025-11-10 14:38:25 -0300

    add initial files

---
+++ frontend/src/app/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  standalone: true,
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  count = 0;
+  onButtonClick(){
+    this.count ++;
+  }
+}
